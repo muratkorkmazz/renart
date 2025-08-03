a5785e8249225a6161d164de529180b63aaf1cad
muratkorkmazz <131611616+muratkorkmazz@users.noreply.github.com>
2025-08-03 20:37:22 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,30 +7,6 @@ const app = express();
 
 
 
-//Ejs setup
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
-
-// Static files
-// app.use(express.static('public'));
-
-
-// cookie parser
-// app.use(cookieParser());
-
-
-
-// Body parser
-
-// Parse application/json
-// app.use(bodyParser.json());
-
-// Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
 
 app.get('/', (req, res) => {
   res.render('index');  
@@ -64,3 +40,4 @@ app.get("/fetch-data",(req,res)=>{
 app.listen(3001, () => {
   console.log('Server running at http://localhost:3001/');
 });
+
